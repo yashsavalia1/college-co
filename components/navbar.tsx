6b3b2c77d6efc328665fdc225592bd62b543747c
yashsavalia1 <yashsavalia1@gmail.com>
2021-10-09 15:08:39 -0500

    navbar update

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -7,12 +7,15 @@ function Navbar() {
                 <img src="/CollegeCo.png" width="50"></img>
             </a>
 
-            <a href="./search">
-                Categories
-            
-            </a>
+            <a href="./search">Furniture</a>
+            <a href="./search">Textbooks/Classroom</a>
+            <a href="./search">Electronics</a>
+            <a href="./search">Apparell</a>
+            <a href="./search">Sports</a>
             <a href="./search">Search</a>
+            <a href="./search">Cart</a>
             <a href="./search">Login</a>
+
 
         </nav>
     )
