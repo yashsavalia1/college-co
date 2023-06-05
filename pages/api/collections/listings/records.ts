3869cfebd8a9306313b4b7a92b2ec0fa39f2d771
Ved Sriraman <sriramanved@gmail.com>
2023-06-05 14:39:07 -0400

    fixed listing creation

--- pages/api/collections/listings/records.ts
+++ pages/api/collections/listings/records.ts
@@ -1,14 +1,16 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { initPocketBase } from '../../../utils/pocketbase-auth';
-import { NewListing } from '../../../types/listing';
+import { initPocketBase } from '../../../../utils/pocketbase-auth';
+import { NewListing } from '../../../../types/listing';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const pb = await initPocketBase(req, res);
+  
   try {
     const listingData: NewListing = {
       title: req.body.title,
       description: req.body.description,
       price: req.body.price,
+      user: req.body.userId,
       published: false,
       created: new Date().toISOString(),
     };
--- pages/listings/create.tsx
+++ pages/listings/create.tsx
@@ -1,5 +1,19 @@
-import { useState } from 'react';
+import { Dispatch, FormEvent, HTMLInputTypeAttribute, SetStateAction, useState } from 'react';
 import { useRouter } from 'next/router';
+import { initPocketBase, usePocketBase } from '../../utils/pocketbase-auth';
+import serializeAuthStore from '../../utils/serialize-authstore';
+import PocketBase, { BaseAuthStore, Record } from 'pocketbase';
+import { GetServerSideProps, InferGetServerSidePropsType, NextApiRequest, NextApiResponse } from 'next';
+
+export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
+  const pb = await initPocketBase(req, res);
+
+  return {
+    props: {
+      authStore: serializeAuthStore(pb.authStore),
+    },
+  };
+};
 
 export default function CreateListing() {
   const [title, setTitle] = useState("");
@@ -7,21 +21,25 @@ export default function CreateListing() {
   const [price, setPrice] = useState(0);
   const [error, setError] = useState("");
   const router = useRouter();
+  const pb = usePocketBase();
+  const model = pb.authStore.model;
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
-    if(title === "" || description === "" || price < 0) {
+    if(title.trim() === "" || description.trim() === "" || price < 0) {
       alert("Please fill in all fields correctly.");
       return;
     }
 
+    console.log(title, description, price, model?.id.toString());
+
     try {
-      const response = await fetch('/api/listings/create', {
+      const response = await fetch('/api/collections/listings/records', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ title, description, price }),
-      });
+        body: JSON.stringify({ title, description, price, userId: model?.id.toString() }),
+      });      
 
       if (response.ok) {
         // Redirect to the home page after the listing is created
